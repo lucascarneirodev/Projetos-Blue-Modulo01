c594b924bb24d3d8776e6e044e67392e8a335688
lucascarneiro-dev <lucas.carneiro098@hotmail.com>
2021-06-13 18:00:51 -0300

    Update detetive.py

--- detetive.py
+++ detetive.py
@@ -5,12 +5,12 @@ print('Jogo detetive')
 
 
 
-pergunta01  =   
+pergunta01  =   input("Você telefonou para a vítima?[S/N]").upper()
 
-pergunta02  =   
+pergunta02  =   input("Você esteve no local do crime?[S/N]").upper()
 
-pergunta03  =   
+pergunta03  =   input("Você mora perto da vítima?[S/N]").upper()
 
-pergunta04  =   
+pergunta04  =   input("Você devia para a vítima?[S/N]").upper()
 
-pergunta05  =   
+pergunta05  =   input("Você já trabalhou com a vítima?[S/N]").upper()
